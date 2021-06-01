a3ade5f7b8ad617872147f54b03838ad71a0e166
salvable <salvable@kakao.com>
2021-06-01 21:32:42 +0900

    react menubar임시작성

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import About from './Stork/test/About';
-import Home from './Stork/test/Home';
-import { Route } from 'react-router-dom';
+import Navbar from "./Stork/component/Navbar";
+import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
 
 function App() {
   return (
       // exact의 의미는 정확히 이 path에서만 해당 라우팅을 이뤄지도록 하라는 명령
-      <div>
-        <Route path="/" component={Home} exact/>
-        <Route path="/about" component={About} />
-      </div>
+      <>
+          <Router>
+            <Navbar />
+            <Switch>
+                <Route path='/' />
+            </Switch>
+          </Router>
+      </>
+
   );
 }
 
---
+++ src/Stork/component/SidebarData.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import * as FaIons from "react-icons/fa"
+import * as AiIcons from "react-icons/ai"
+import * as IoIcons from "react-icons/io"
+
+export const SidebarData = [
+    {
+        title: 'Home',
+        path: '/',
+        icon: <AiIcons.AiFillHome />,
+        cName: 'nav-text'
+    },
+    {
+        title: 'Reports',
+        path: '/reports',
+        icon: <IoIcons.IoIosPaper />,
+        cName: 'nav-text'
+    }
+]
