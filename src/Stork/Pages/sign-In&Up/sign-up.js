449a4391f93d76e96c2e24aab6fd13d1388ecfdc
salvable <salvable@kakao.com>
2021-06-23 15:20:05 +0900

    copyright 추가

--- src/Stork/Pages/sign-In&Up/sign-up.js
+++ src/Stork/Pages/sign-In&Up/sign-up.js
@@ -13,6 +13,19 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 
+function Copyright() {
+    return (
+        <Typography variant="body2" color="textSecondary" align="center">
+            {'Copyright © '}
+            <Link color="inherit" href="https://material-ui.com/">
+                Your Website
+            </Link>{' '}
+            {new Date().getFullYear()}
+            {'.'}
+        </Typography>
+    );
+}
+
 const useStyles = makeStyles((theme) => ({
     paper: {
         marginTop: theme.spacing(8),
@@ -138,6 +151,7 @@ export default function SignUp() {
                 </form>
             </div>
             <Box mt={5}>
+                <Copyright />
             </Box>
         </Container>
     );
