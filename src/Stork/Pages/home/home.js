ca9f8c5a7882304a4a5b483e27c961bdca027217
salvable <salvable@kakao.com>
2021-07-09 17:06:57 +0900

    home 화면 기본 테이블 작성

--- src/Stork/Pages/home/home.js
+++ src/Stork/Pages/home/home.js
@@ -1,18 +1,125 @@
-import React from "react";
+import React, { Component } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import {Table, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
 
 const Home = () => {
+
+    const styles = theme => ({
+        root: {
+            padding: theme.spacing(3),
+            background: '#eeeeee'
+        },
+        paper: {
+            padding: theme.spacing(3),
+            textAlign: 'center',
+            color: theme.palette.text.primary,
+        },
+        table: {
+            minWidth: 650,
+        }
+    });
+
     return (
-        <div
-            style = {{
-                display: 'flex',
-                justifyContent:'center',
-                alignItems: 'center',
-                height: '90vh'
-            }}
-        >
-            <h1>Home</h1>
+        <div className={styles.root}>
+            <Grid container spacing={10}>
+                <Grid item xs={4}>
+                    <TableContainer component={Paper}>
+                        <div style = {{
+                            display: 'flex',
+                            justifyContent:'center',
+                            alignItems: 'center',
+                        }}>
+                        <h3>상한가 </h3>
+                        </div>
+                        <Table className={styles.table} aria-label="simple table" >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">종목명</TableCell>
+                                    <TableCell align="center">현재가</TableCell>
+                                    <TableCell align="center">전일비</TableCell>
+                                    <TableCell align="center">등락률</TableCell>
+
+                                </TableRow>
+                            </TableHead>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item xs={4}>
+                    <TableContainer component={Paper}>
+                        <div style = {{
+                            display: 'flex',
+                            justifyContent:'center',
+                            alignItems: 'center',
+                        }}>
+                            <h3>거래 상위</h3>
+                        </div>
+                        <Table className={styles.table} aria-label="simple table" >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">종목명</TableCell>
+                                    <TableCell align="center">현재가</TableCell>
+                                    <TableCell align="center">전일비</TableCell>
+                                    <TableCell align="center">등락률</TableCell>
+
+                                </TableRow>
+                            </TableHead>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item xs={4}>
+                    <Paper className={styles.paper}>Content 3</Paper>
+                </Grid>
+                <Grid item xs={4}>
+                    <TableContainer component={Paper}>
+                        <div style = {{
+                            display: 'flex',
+                            justifyContent:'center',
+                            alignItems: 'center',
+                        }}>
+                            <h3>하한가 </h3>
+                        </div>
+                        <Table className={styles.table} aria-label="simple table" >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">종목명</TableCell>
+                                    <TableCell align="center">현재가</TableCell>
+                                    <TableCell align="center">전일비</TableCell>
+                                    <TableCell align="center">등락률</TableCell>
+
+                                </TableRow>
+                            </TableHead>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+                <Grid item xs={4}>
+                    <TableContainer component={Paper}>
+                        <div style = {{
+                            display: 'flex',
+                            justifyContent:'center',
+                            alignItems: 'center',
+                        }}>
+                            <h3>시가총액 상위</h3>
+                        </div>
+                        <Table className={styles.table} aria-label="simple table" >
+                            <TableHead>
+                                <TableRow>
+                                    <TableCell align="center">종목명</TableCell>
+                                    <TableCell align="center">현재가</TableCell>
+                                    <TableCell align="center">전일비</TableCell>
+                                    <TableCell align="center">등락률</TableCell>
+
+                                </TableRow>
+                            </TableHead>
+                        </Table>
+                    </TableContainer>
+                </Grid>
+
+
+            </Grid>
         </div>
-    )
+    );
 };
 
 export default Home
