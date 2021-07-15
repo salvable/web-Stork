465eb524f065761890e0a6161de9e1ade7bce903
salvable <salvable@kakao.com>
2021-07-15 16:04:58 +0900

    Token이 있으나 유효기간이 만료되었을때 refreshToken을 이용하여 토큰을 재발급 받아 Local Storage에 저장하도록 설정

--- src/Stork/component/Navbar/index.js
+++ src/Stork/component/Navbar/index.js
@@ -4,11 +4,27 @@ import { TextField, InputAdornment } from '@material-ui/core'
 import SearchIcon from "@material-ui/icons/Search";
 import axios from "axios";
 import Button from "@material-ui/core/Button";
+import {useHistory} from "react-router";
 
 
 const Navbar = () => {
+    const history = useHistory()
     const [isLogin,setIsLogin] = useState(false)
     const [userId, setUserId] = useState("")
+
+    const getUserId = async(userId) => {
+        const user = await axios.get(`http://localhost:3000/getUser/${userId}`)
+
+        // userId를 리턴
+        return user.data.user.userId
+    }
+
+    const LogOut = () => {
+        localStorage.removeItem('accessToken')
+        localStorage.removeItem('refreshToken')
+        alert("로그아웃이 완료되었습니다.")
+        window.location.replace('/')
+    }
 
     useEffect(()=>{
         async function CheckAuth(token){
@@ -17,13 +33,17 @@ const Navbar = () => {
                     headers: {
                         Authorization: `Bearer ${token}`
                     }})
-
                 if(response.status == 200){
                     setIsLogin(true)
-
-                    const user = await axios.get(`http://localhost:3000/getUser/${response.data.userId}`)
-                    setUserId(user.data.user.userId)
-
+                    const userId = await getUserId(response.data.userId)
+                    setUserId(userId)
+                }else{
+                    console.log("#####")
+                    const refresh = await axios.get("http://localhost:3000/refreshToken")
+                    window.localStorage.setItem("accessToken", refresh.data.token)
+                    window.localStorage.setItem("refreshToken", refresh.data.refreshToken)
+                    const userId = await getUserId(response.data.userId)
+                    setUserId(userId)
                 }
             }
         }
@@ -31,13 +51,6 @@ const Navbar = () => {
         const Token = localStorage.getItem("accessToken")
         CheckAuth(Token)
     },[isLogin])
-
-    const LogOut = () => {
-        localStorage.removeItem('accessToken')
-        localStorage.removeItem('refreshToken')
-        alert("로그아웃이 완료되었습니다.")
-        window.location.replace('/')
-    }
 
     return (
         <>
@@ -79,7 +92,7 @@ const Navbar = () => {
 
                <NavMenu>
                    <React.Fragment>
-                       {isLogin ? (<Button variant="contained" color="primary">My Info</Button>) : (<NavLink to='/signIn'>Sign In</NavLink>)}
+                       {isLogin ? (<Button variant="contained" color="primary" onClick={() => {history.push(`/profile/${userId}`)}}>My Info</Button>) : (<NavLink to='/signIn'>Sign In</NavLink>)}
                    </React.Fragment>
                    <React.Fragment>
                        {isLogin ? (<Button variant="contained" color="primary" onClick={() => {LogOut()}}>LogOut</Button>) : (<NavLink to='/signUp'>Sign Up</NavLink>)}
