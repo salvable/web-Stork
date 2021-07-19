cfb00aea7d2272eff101692c3af9b5b409ffdf7f
salvable <salvable@kakao.com>
2021-07-19 19:55:08 +0900

    profile 기본 form 작성

--- src/Stork/Pages/profile/profile.js
+++ src/Stork/Pages/profile/profile.js
@@ -1,16 +1,36 @@
 import React from "react";
+import Grid from '@material-ui/core/Grid';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import Home from "../home/home";
 
 const Profile = () => {
+
+    const styles = theme => ({
+        root: {
+            padding: theme.spacing(3),
+            background: '#eeeeee'
+        },
+        paper: {
+            padding: theme.spacing(3),
+            textAlign: 'center',
+            color: theme.palette.text.primary,
+        },
+    });
+
+
     return (
-        <div
-            style = {{
-                display: 'flex',
-                justifyContent:'center',
-                alignItems: 'center',
-                height: '90vh'
-            }}
-        >
-            <h1>Profile</h1>
+        <div className={styles.root}>
+            <h1>My Info</h1>
+            <Grid container spacing={3} style:{margin-top:40px}>
+                <Grid item xs={5}>
+                    <Paper className={styles.paper} elevation={3}>Content 2</Paper>
+                </Grid>
+                <Grid item xs={5}>
+                    <Paper className={styles.paper} elevation={3}>Content 3</Paper>
+                </Grid>
+
+            </Grid>
         </div>
     )
 };
