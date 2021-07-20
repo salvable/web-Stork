417e7a3ce2a172f0e8478daa78cf0a7cbae96769
salvable <salvable@kakao.com>
2021-07-20 19:44:26 +0900

    profile 기본 form변경

--- src/Stork/Pages/profile/profile.js
+++ src/Stork/Pages/profile/profile.js
@@ -1,48 +1,135 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
-import Paper from "@material-ui/core/Paper";
-import Home from "../home/home";
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import axios from "axios";
 
-const Profile = () => {
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
 
-    const styles = theme => ({
-        root: {
-            marginTop: 5,
-            padding: theme.spacing(3),
-            background: '#eeeeee'
-        },
-        paper: {
-            padding: theme.spacing(3),
-            textAlign: 'center',
-            color: theme.palette.text.primary,
-        },
-    });
+export default function SignUp() {
+    const classes = useStyles();
+    const [id,setId] = useState("")
+    const [email,setEmail] = useState("")
+    const [name, setName] = useState("")
+    const [phoneNumber, setPhoneNumber] = useState("")
 
 
     return (
-        <div className={styles.root}>
-            <Grid container spacing={8}>
-                <Grid item xs={6}>
-                    <Paper className={styles.paper} elevation={3}>
-                        <div>내 정보</div>
-                    </Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <Paper className={styles.paper} elevation={3}>
-                        <h3>내 계좌</h3>
-                    </Paper>
-                </Grid>
-                <Grid item xs={6}>
-                    <Paper className={styles.paper} elevation={3}>
-                        <h3>내 등급</h3>
-                    </Paper>
-                </Grid>
+        <Container component="main" maxWidth="xs">
+            <div className={classes.paper}>
+                <Avatar className={classes.avatar} />
 
+                <Typography component="h1" variant="h5">
+                    My Info
+                </Typography>
+                <form className={classes.form} noValidate>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            <TextField
+                                autoComplete="fname"
+                                name="ID"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="ID"
+                                label="ID"
+                                value={id}
+                                autoFocus
+                                onChange={(e)=> {
+                                    setId(e.target.value)
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="Email Address"
+                                name="email"
+                                autoComplete="email"
+                                value={email}
+                                onChange={(e)=> {
+                                    setEmail(e.target.value)
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="name"
+                                label="name"
+                                id="name"
+                                value={name}
+                                onChange={(e)=> {
+                                    setName(e.target.value)
+                                }}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                name="phoneNumber"
+                                label="phoneNumber"
+                                type="text"
+                                id="phoneNumber"
+                                autoComplete="phoneNumber"
+                                value={phoneNumber}
+                                onChange={(e)=> {
+                                    const regex = /^[0-9\b -]{0,13}$/;
+                                    if (regex.test(e.target.value)) {
+                                        if(e.target.value.length == 3 || e.target.value.length === 8){
+                                            setPhoneNumber(e.target.value + "-");
+                                        }else{
+                                            setPhoneNumber(e.target.value);
+                                        }
+                                    }
+                                }}
+                                onKeyDown={(e)=>{
+                                    if(!(e.keyCode >= 48 && e.keyCode <= 57) && e.keyCode === 8){
+                                        if((e.target.value.length === 9 || e.target.value.length === 4) && phoneNumber.slice(e.target.value.length-1,e.target.value.length) == "-"){
+                                            setPhoneNumber(phoneNumber.slice(0,e.target.value.length-1))
+                                        }
+                                    }
+                                }}
+                            />
+                        </Grid>
+                    </Grid>
+                </form>
+            </div>
 
-            </Grid>
-        </div>
-    )
-};
-
-export default Profile
+        </Container>
+    );
+}
