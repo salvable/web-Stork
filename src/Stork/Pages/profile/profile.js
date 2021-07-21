96eaeb8b0431f7d3e7af7132e6d30001827d46cd
salvable <salvable@kakao.com>
2021-07-21 20:25:30 +0900

    Modify 버튼 추가, 버튼을 누를시 pw를 확인하는 페이지로 이동

--- src/Stork/Pages/profile/profile.js
+++ src/Stork/Pages/profile/profile.js
@@ -1,18 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
 import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import axios from "axios";
-import Home from "../home/home";
+import {useHistory} from "react-router";
 
 const useStyles = makeStyles((theme) => ({
     paper: {
@@ -35,6 +30,7 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Profile = ({match}) => {
+    const history = useHistory()
     const classes = useStyles();
     const userId = match.params.userId
 
@@ -128,6 +124,18 @@ const Profile = ({match}) => {
                                 }}
                             />
                         </Grid>
+                        <Button
+                        fullWidth
+                        variant="contained"
+                        color="secondary"
+                        className={classes.submit}
+                        onClick={(e)=>{
+                          history.push(`/checkInfo/${userId}`)
+                        }}
+                    >
+                        Modify Info
+                    </Button>
+
                     </Grid>
                 </form>
             </div>
