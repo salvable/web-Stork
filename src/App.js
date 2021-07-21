666d05e26303db46d16e555c2d23493d65224d61
salvable <salvable@kakao.com>
2021-07-21 20:36:40 +0900

    checkInfo 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import Home from "./Stork/Pages/home/home";
 import Stork from "./Stork/Pages/stork/stork";
 import BitCoin from "./Stork/Pages/bitCoin/bitCoin";
-import signUp from "./Stork/Pages/sign-In&Up/sign-up"
-import signIn from "./Stork/Pages/sign-In&Up/sign-in"
-import favorite from "./Stork/Pages/favorite/favorite"
-import contact from "./Stork/Pages/contact-us/contact-us"
-import profile from "./Stork/Pages/profile/profile"
+import SignUp from "./Stork/Pages/sign-In&Up/sign-up"
+import SignIn from "./Stork/Pages/sign-In&Up/sign-in"
+import Favorite from "./Stork/Pages/favorite/favorite"
+import Contact from "./Stork/Pages/contact-us/contact-us"
+import Profile from "./Stork/Pages/profile/profile"
+import CheckInfo from "./Stork/Pages/profile/checkInfo";
+
 function App() {
   return (
       <Router>
@@ -18,11 +20,12 @@ function App() {
               <Route path="/" exact component={Home} />
               <Route path="/stork" component={Stork} />
               <Route path="/bitCoin" component={BitCoin} />
-              <Route path="/signUp" component={signUp} />
-              <Route path="/signIn" component={signIn} />
-              <Route path="/favorite" component={favorite}/>
-              <Route path="/contact-us" component={contact}/>
-              <Route path="/profile/:userId" component={profile}/>
+              <Route path="/signUp" component={SignUp} />
+              <Route path="/signIn" component={SignIn} />
+              <Route path="/favorite" component={Favorite}/>
+              <Route path="/contact-us" component={Contact}/>
+              <Route path="/profile/:userId" component={Profile}/>
+              <Route path="/checkInfo/:userId" component={CheckInfo}/>
           </Switch>
       </Router>
   );
