000282cfaa8b3f8ba340c1b6463e3440a0dd74f0
salvable <salvable@kakao.com>
2021-08-11 20:01:03 +0900

    code 리팩토링 및 bitcoin Page 기본 폼 작성

--- src/Stork/Pages/bitCoin/bitCoin.js
+++ src/Stork/Pages/bitCoin/bitCoin.js
@@ -11,44 +11,46 @@ import {
     TableRow,
     TextField
 } from "@material-ui/core";
-import axios from "axios";
-import TableScrollbar from 'react-table-scrollbar'
-import SearchIcon from "@material-ui/icons/Search";
 
-const Bitcoin = () => {
+import BitCoinTable from "./bitcoinTable/table";
+import chart from "../../Chart/storkChart.png";
+import ChatLog from "../../component/chat/chatLog";
+import ChatInput from "../../component/chat/chatInput";
+import socketIOClient from "socket.io-client";
 
-    const styles = theme => ({
-        root: {
-            padding: theme.spacing(3),
-            background: '#eeeeee',
-            margin: '0.8rem'
-        },
-        paper: {
-            padding: theme.spacing(3),
-            textAlign: 'center',
-            color: theme.palette.text.primary,
-            margin: '0.8rem'
-        },
-        table: {
-            minWidth: 650,
-        }
-    });
+const Bitcoin = ({match}) => {
 
-    // 각각 상한가, 하한가, 거래상위, 시가총액 상위를 나타냄, 네이밍은 네이버 주식 url로 결정
-    const [coinList,setCoinList] = useState([])
+    const [storkName,setStorkName] = useState("")
+    const [storkPrice,setStorkPrice] = useState([])
+    const [currentSocket, setCurrentSocket] = useState()
+
+    const userId = localStorage.getItem("userId")
+
+    const myInfo = {
+        roomName: match.params.storkId ? match.params.storkId : "005930",
+        userName: userId ? userId : "guest",
+    };
+
 
     useEffect(() => {
-        async function getCoinList(){
-            const response = await axios.get("http://localhost:8000/api_bit/bitCoin/getBitcoinList")
-            setCoinList(response.data.bitcoin)
+        if (currentSocket) {
+            currentSocket.on("connect", () => {
+                currentSocket.emit("join", myInfo);
+            });
+        }else{
+            setCurrentSocket(socketIOClient("localhost:3002"));
         }
+    }, [currentSocket]);
 
-        getCoinList()
-    },[]);
 
-    const getCoinList = async (search) =>{
-        const response = await axios.get(`http://localhost:8000/api_bit/bitCoin/getBitcoinList/${search}`)
-        setCoinList(response.data.bitcoin)
+
+    const setStorkColor = () =>{
+        const str = String(storkPrice.variance)
+
+        if(str.indexOf("하락") != -1){
+            return {color: "blue"}
+        }
+        return {color: "red"}
     }
 
     return (
@@ -58,78 +60,35 @@ const Bitcoin = () => {
                     <Table aria-label="simple table" >
                         <TableHead>
                             <TableRow>
-                                <TableCell align="center">종목명</TableCell>
-                                <TableCell align="center">현재가</TableCell>
-                                <TableCell align="center">전일비</TableCell>
-                                <TableCell align="center">등락률</TableCell>
+                                <TableCell align="left" colSpan={3}><h1>{storkName}</h1></TableCell>
                             </TableRow>
                         </TableHead>
                         <TableBody>
-                            {/*{siseUpper.map((row) => (*/}
-                            {/*    <TableRow key={row[0]}>*/}
-                            {/*        <TableCell align="center">{row[0]}</TableCell>*/}
-                            {/*        <TableCell align="center">{row[1]}</TableCell>*/}
-                            {/*        <TableCell align="center">{row[2]}</TableCell>*/}
-                            {/*        <TableCell align="center">{row[3]}</TableCell>*/}
-                            {/*    </TableRow>*/}
-                            {/*))}*/}
+                            <TableRow>
+                                <TableCell rowSpan={2} style={{ width: "50%" }}><h1 style={setStorkColor()}>{storkPrice.price}</h1><h4>전일대비 {storkPrice.variance}</h4></TableCell>
+                                <TableCell style={{ width: "25%" ,color: "red"}}>고가 {storkPrice.highPrice}</TableCell>
+                                <TableCell style={{ width: "25%" }}>거래량</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell style={{ width: "25%" ,color: "blue"}}>저가 {storkPrice.lowPrice}</TableCell>
+                                <TableCell style={{ width: "25%" }}>거래대금</TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell colSpan={2}><img src={chart}></img></TableCell>
+                                <TableRow>
+                                    <TableCell height={600} >
+                                        <h3>채팅방</h3>
+                                        <ChatLog socket={currentSocket}></ChatLog>
+                                        <ChatInput userId={userId} socket={currentSocket}></ChatInput>
+                                    </TableCell>
+                                </TableRow>
+                            </TableRow>
                         </TableBody>
                     </Table>
                 </TableContainer>
             </Grid>
-
             <Grid item xs={4}>
-                <TableContainer component={Paper}>
-                    <Table aria-label="simple table" >
-                        <TableHead>
-                            <TableRow>
-                                <TextField
-                                    variant="outlined"
-                                    placeholder="CoinName Search"
-                                    InputProps={{
-                                        endAdornment: (
-                                            <InputAdornment position="end">
-                                                <SearchIcon />
-                                            </InputAdornment>
-                                        ),
-                                        style:{
-                                            backgroundColor: "white",
-                                            color: "black",
-                                            width: "195%",
-                                            margin: '5px'
-
-                                        }
-                                    }}
-                                    onChange={async (e)=>{
-                                        await getCoinList(e.target.value)
-                                    }}
-                                />
-                            </TableRow>
-                        </TableHead>
-                    </Table>
-                </TableContainer>
-                <TableContainer component={Paper}>
-                    <TableScrollbar rows={30}>
-                        <Table className={styles.table} aria-label="simple table" stickyHeader>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell align="center">마켓 코드</TableCell>
-                                    <TableCell align="center">코인 이름</TableCell>
-                                    <TableCell align="center">english name</TableCell>
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {coinList.map((row) => (
-                                    <TableRow key={row.stork_id}>
-                                        <TableCell align="center">{row.market}</TableCell>
-                                        <TableCell align="center">{row.name}</TableCell>
-                                        <TableCell align="center">{row.eng_name}</TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableScrollbar>
-                </TableContainer>
+                <BitCoinTable></BitCoinTable>
             </Grid>
         </Grid>
     );
---
+++ src/Stork/Pages/bitCoin/bitcoinTable/table.js
@@ -0,0 +1,109 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import {
+    InputAdornment,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TextField
+} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import SearchIcon from "@material-ui/icons/Search";
+import TableScrollbar from "react-table-scrollbar";
+
+const BitCoinTable = () => {
+    const styles = theme => ({
+        root: {
+            padding: theme.spacing(3),
+            background: '#eeeeee',
+            margin: '0.8rem'
+        },
+        paper: {
+            padding: theme.spacing(3),
+            textAlign: 'center',
+            color: theme.palette.text.primary,
+            margin: '0.8rem'
+        },
+        table: {
+            minWidth: 650,
+        }
+    });
+    // 각각 상한가, 하한가, 거래상위, 시가총액 상위를 나타냄, 네이밍은 네이버 주식 url로 결정
+    const [coinList,setCoinList] = useState([])
+
+    useEffect(() => {
+        async function getCoinList(){
+            const response = await axios.get("http://localhost:8000/api_bit/bitCoin/getBitcoinList")
+            setCoinList(response.data.bitcoin)
+        }
+
+        getCoinList()
+    },[]);
+
+    const getCoinList = async (search) =>{
+        const response = await axios.get(`http://localhost:8000/api_bit/bitCoin/getBitcoinList/${search}`)
+        setCoinList(response.data.bitcoin)
+    }
+
+    return (
+        <Grid container spacing={6} style={{height: "100%", marginTop: 1}}>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table" >
+                    <TableHead>
+                        <TableRow>
+                            <TextField
+                                variant="outlined"
+                                placeholder="CoinName Search"
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <SearchIcon />
+                                        </InputAdornment>
+                                    ),
+                                    style:{
+                                        backgroundColor: "white",
+                                        color: "black",
+                                        width: "195%",
+                                        margin: '5px'
+
+                                    }
+                                }}
+                                onChange={async (e)=>{
+                                    await getCoinList(e.target.value)
+                                }}
+                            />
+                        </TableRow>
+                    </TableHead>
+                </Table>
+            </TableContainer>
+            <TableContainer component={Paper}>
+                <TableScrollbar rows={30}>
+                    <Table className={styles.table} aria-label="simple table" stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">마켓 코드</TableCell>
+                                <TableCell align="center">코인 이름</TableCell>
+                                <TableCell align="center">english name</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {coinList.map((row) => (
+                                <TableRow key={row.stork_id}>
+                                    <TableCell align="center">{row.market}</TableCell>
+                                    <TableCell align="center">{row.name}</TableCell>
+                                    <TableCell align="center">{row.eng_name}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableScrollbar>
+            </TableContainer>
+        </Grid>
+    );
+};
+
+export default BitCoinTable
