71dd4d6927d65b5555168827da78663ce5107fe9
salvable <salvable@kakao.com>
2021-08-15 19:40:29 +0900

    favorite 기본 form 및 favoriteList를 가져오기 성공

--- src/Stork/Pages/favorite/favorite.js
+++ src/Stork/Pages/favorite/favorite.js
@@ -1,18 +1,195 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+import {
+    InputAdornment,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TextField
+} from "@material-ui/core";
 
-const Favorite = () => {
+import FavoriteTable from "./table/favoriteTable";
+import chart from "../../Chart/coinChart.png";
+import ChatLog from "../../component/chat/chatLog";
+import ChatInput from "../../component/chat/chatInput";
+import socketIOClient from "socket.io-client";
+import axios from "axios";
+import {useHistory} from "react-router";
+import StarIcon from "@material-ui/icons/Star";
+import StarBorderIcon from "@material-ui/icons/StarBorder";
+import Button from "@material-ui/core/Button";
+
+const Favorite = ({match}) => {
+
+    const [bitCoin,setBitCoin] = useState("")
+    const [bitCoinId,setBitCoinId] = useState("")
+    const [currentSocket, setCurrentSocket] = useState()
+    const [isExistFavorite, setIsExistFavorite] = useState(false)
+
+    const token = localStorage.getItem('accessToken')
+    const userId = localStorage.getItem("userId")
+    const history = useHistory()
+
+    const myInfo = {
+        roomName: match.params.bitCoinId ? match.params.bitCoinId : "KRW-BTC",
+        userName: userId ? userId : "guest",
+    };
+
+    useEffect(() => {
+        async function getBitcoin(){
+            if(match.params.bitCoinId == undefined){
+                const response = await axios.get(`http://localhost:8000/api_bit/bitCoin/getBitcoinPrice/KRW-BTC`)
+                setBitCoinId("KRW-BTC")
+                setBitCoin(response.data)
+            }else{
+                try {
+                    const response = await axios.get(`http://localhost:8000/api_bit/bitCoin/getBitcoinPrice/${match.params.bitCoinId}`)
+                    setBitCoinId(match.params.bitCoinId)
+                    setBitCoin(response.data)
+                }catch (e){
+                    alert("상장 폐지된 코인입니다!")
+                    history.goBack()
+                }
+            }
+        }
+        getBitcoin()
+    }, []);
+
+    useEffect(() => {
+        async function getStorkChart(){
+            if(match.params.bitCoinId == undefined){
+                const response = await axios.get(`http://localhost:8000/api_bit/bitCoin/getBitcoinChart/KRW-BTC`)
+            }else{
+                const response = await axios.get(`http://localhost:8000/api_bit/bitCoin/getBitcoinChart/${match.params.bitCoinId}`)
+            }
+        }
+        getStorkChart()
+    },[]);
+
+    useEffect(() => {
+        async function getFavorite(){
+            const response = await axios.get(`http://localhost:3000/favorite/${userId}?favoriteId=${bitCoinId}`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }})
+            if(response.data.favorite != null){
+                setIsExistFavorite(true)
+            }
+        }
+        getFavorite()
+    },[]);
+
+    useEffect(() => {
+        if (currentSocket) {
+            currentSocket.on("connect", () => {
+                currentSocket.emit("join", myInfo);
+            });
+        }else{
+            setCurrentSocket(socketIOClient("localhost:3002"));
+        }
+    }, [currentSocket]);
+
+    const setStorkColor = () =>{
+        const str = String(bitCoin.change)
+
+        if(str.indexOf("FALL") != -1){
+            return {color: "blue"}
+        }
+        return {color: "red"}
+    }
+
+    const setChange = (change) =>{
+        if(change == "FALL"){
+            return " 하락"
+        }else if (change == "EVEN"){
+            return " 보합"
+        }else{
+            return " 상승"
+        }
+    }
+
+    const setIcon = () =>{
+        if(isExistFavorite == true){
+            return <StarIcon/>
+        }
+        return <StarBorderIcon/>
+    }
+
+    const setFavorite = async() =>{
+        if(isExistFavorite == true){
+            const response = await axios.delete(`http://localhost:3000/favorite/${userId}?favoriteId=${bitCoinId}`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }})
+
+            setIsExistFavorite(false)
+            alert("즐겨찾기에서 삭제되었습니다.")
+            return ;
+        }
+
+        const response = await axios.post(`http://localhost:3000/favorite/${userId}`,{
+            favoriteId: bitCoinId,
+            favoriteName: bitCoin.name,
+        }, {
+            headers: {
+                Authorization: `Bearer ${token}`
+            }})
+        setIsExistFavorite(true)
+        alert("즐겨찾기에 추가되었습니다.")
+    }
+
     return (
-        <div
-            style = {{
-                display: 'flex',
-                justifyContent:'center',
-                alignItems: 'center',
-                height: '90vh'
-            }}
-        >
-            <h1>Favorite</h1>
-        </div>
-    )
+        <Grid container spacing={6} style={{height: "100%", marginTop: 1}}>
+            <Grid item xs={8}>
+                <TableContainer component={Paper}>
+                    <Table aria-label="simple table" >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="left" colSpan={3}><h1>{bitCoin.name}</h1></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell rowSpan={2} style={{ width: "50%" }}><h1 style={setStorkColor()}>{bitCoin.price}</h1><h4>전일대비 {bitCoin.change_price}{setChange(bitCoin.change)}</h4></TableCell>
+                                <TableCell style={{ width: "25%" ,color: "red"}}>고가 {bitCoin.high_price}</TableCell>
+                                <TableCell style={{ width: "25%" }}>거래 : <Button variant="contained" color="primary">매수</Button> <Button variant="contained" color="secondary">매도</Button></TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell style={{ width: "25%" ,color: "blue"}}>저가 {bitCoin.low_price}</TableCell>
+                                <TableCell style={{ width: "25%" }}>즐겨찾기 :
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={async()=>{
+                                            await setFavorite()
+                                        }}>
+                                        {setIcon()}
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell colSpan={2}><img src={chart}></img></TableCell>
+                                <TableRow>
+                                    <TableCell height={600} >
+                                        <h3>채팅방</h3>
+                                        <ChatLog socket={currentSocket}></ChatLog>
+                                        <ChatInput userId={userId} socket={currentSocket}></ChatInput>
+                                    </TableCell>
+                                </TableRow>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+            <Grid item xs={4}>
+                <FavoriteTable userId={userId} token={token}></FavoriteTable>
+            </Grid>
+        </Grid>
+    );
 };
 
 export default Favorite
---
+++ src/Stork/Pages/favorite/table/favoriteTable.js
@@ -0,0 +1,110 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import Grid from "@material-ui/core/Grid";
+import {
+    InputAdornment,
+    Table,
+    TableBody,
+    TableCell,
+    TableContainer,
+    TableHead,
+    TableRow,
+    TextField
+} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import SearchIcon from "@material-ui/icons/Search";
+import TableScrollbar from "react-table-scrollbar";
+
+const FavoriteTable = ({userId,token}) => {
+    const styles = theme => ({
+        root: {
+            padding: theme.spacing(3),
+            background: '#eeeeee',
+            margin: '0.8rem'
+        },
+        paper: {
+            padding: theme.spacing(3),
+            textAlign: 'center',
+            color: theme.palette.text.primary,
+            margin: '0.8rem'
+        },
+        table: {
+            minWidth: 650,
+        }
+    });
+    // 각각 상한가, 하한가, 거래상위, 시가총액 상위를 나타냄, 네이밍은 네이버 주식 url로 결정
+    const [favoriteList,setFavoriteList] = useState([])
+
+
+    useEffect(() => {
+        async function getFavoriteList(){
+            const response = await axios.get(`http://localhost:3000/favorites/${userId}`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }})
+            setFavoriteList(response.data.favorites)
+        }
+
+        getFavoriteList()
+    },[]);
+
+
+    return (
+        <Grid container spacing={6} style={{height: "100%", marginTop: 1}}>
+            <TableContainer component={Paper}>
+                <Table aria-label="simple table" >
+                    <TableHead>
+                        <TableRow>
+                            <TextField
+                                variant="outlined"
+                                placeholder="CoinName Search"
+                                InputProps={{
+                                    endAdornment: (
+                                        <InputAdornment position="end">
+                                            <SearchIcon />
+                                        </InputAdornment>
+                                    ),
+                                    style:{
+                                        backgroundColor: "white",
+                                        color: "black",
+                                        width: "195%",
+                                        margin: '5px'
+
+                                    }
+                                }}
+                                onChange={async (e)=>{
+                                    // await getCoinList(e.target.value)
+                                }}
+                            />
+                        </TableRow>
+                    </TableHead>
+                </Table>
+            </TableContainer>
+            <TableContainer component={Paper}>
+                <TableScrollbar rows={30}>
+                    <Table className={styles.table} aria-label="simple table" stickyHeader>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="center">종목 코드</TableCell>
+                                <TableCell align="center">코인 이름</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+
+                            {favoriteList.map((row) => (
+                                <TableRow onClick={() => {
+                                    window.location.href = "/favorite/" + row.favoriteId
+                                }}>
+                                    <TableCell align="center">{row.favoriteId}</TableCell>
+                                    <TableCell align="center">{row.favoriteName}</TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableScrollbar>
+            </TableContainer>
+        </Grid>
+    );
+};
+
+export default FavoriteTable
