29f660e24e89460f6aea29c90a6e2489e414894e
salvable <salvable@kakao.com>
2021-08-25 19:37:27 +0900

    pagination 관련 소스 작성

--- src/Stork/Pages/contact-us/contact-us.js
+++ src/Stork/Pages/contact-us/contact-us.js
@@ -1,16 +1,25 @@
 import React, {useEffect, useState} from "react";
 import Grid from "@material-ui/core/Grid";
-import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import {Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
 import Paper from "@material-ui/core/Paper"
 import Button from "@material-ui/core/Button";
 import ChatLog from "../../component/chat/chatLog";
 import BorderChatInput from "../../component/chat/borderChatInput";
 import socketIOClient from "socket.io-client";
 import axios from "axios";
+import {Pagination} from "@material-ui/lab";
+import {useHistory} from "react-router";
+import queryString from "query-string";
 
-const Contact = () => {
+const Contact = ({location}) => {
+    const history = useHistory()
+    const parsed = queryString.parse(location.search);
+
     const [currentSocket, setCurrentSocket] = useState()
     const [boards,setBoards] = useState([])
+    const [totalCount, setTotalCount] = useState(0)
+    const [page,setPage] = useState(parsed.page ? parsed.page: 1)
+    console.log(page)
 
     const token = localStorage.getItem('accessToken')
     const userId = localStorage.getItem("userId")
@@ -19,6 +28,10 @@ const Contact = () => {
         roomName: "Border",
         userName: userId ? userId : "guest",
     };
+
+    useEffect(() => {
+        setPage(parsed.page ? parsed.page: 1)
+    }, [location.search]);
 
     useEffect(() => {
         if (currentSocket) {
@@ -32,14 +45,17 @@ const Contact = () => {
 
     useEffect(() => {
         async function getBoards(){
-            const response = await axios.get(`http://localhost:3000/boards?page=1`,{
+            const response = await axios.get(`http://localhost:3000/boards?page=${page}`,{
                 headers: {
                     Authorization: `Bearer ${token}`
                 }})
             setBoards(response.data.board)
+            setTotalCount(response.data.totalCount)
         }
         getBoards()
-    }, []);
+    }, [page]);
+
+
 
     const loginCheck = async(accessToken) => {
         if(accessToken){
@@ -61,7 +77,18 @@ const Contact = () => {
         }
     }
 
+    const setPageCount = () =>{
+        if(totalCount % 10 == 0){
+            return parseInt(totalCount / 10)
+        }
+
+        return parseInt(totalCount / 10) + 1
+    }
+
+
+
     return (
+        <div>
         <Grid container spacing={6} style={{height: "100%", marginTop: 1}}>
             <Grid item xs={8} >
                 <TableContainer component = {Paper}>
@@ -74,11 +101,11 @@ const Contact = () => {
                         <TableBody>
                             <TableRow>
                                 <TableCell align="center" style={{width: "10%"}}>글 번호</TableCell>
-                                <TableCell align="center" style={{width: "50%"}}>제목</TableCell>
+                                <TableCell align="center" style={{width: "46%"}}>제목</TableCell>
                                 <TableCell align="center" style={{width: "10%"}}>작성자</TableCell>
                                 <TableCell align="center" style={{width: "10%"}}>날짜</TableCell>
-                                <TableCell align="center" style={{width: "10%"}}>조회수</TableCell>
-                                <TableCell align="center" style={{width: "10%"}}>추천수</TableCell>
+                                <TableCell align="center" style={{width: "12%"}}>조회수</TableCell>
+                                <TableCell align="center" style={{width: "12%"}}>추천수</TableCell>
                             </TableRow>
 
                             {boards.map((row) => (
@@ -88,23 +115,42 @@ const Contact = () => {
                                     <TableCell align="center">{row.boardId}</TableCell>
                                     <TableCell align="center">{row.name}</TableCell>
                                     <TableCell align="center">{row.writer}</TableCell>
-                                    <TableCell align="center">{}</TableCell>
+                                    <TableCell align="center">{row.createdAt.split("T")[0]}</TableCell>
                                     <TableCell align="center">{row.hit}</TableCell>
                                     <TableCell align="center">{row.star}</TableCell>
                                 </TableRow>
                             ))}
+                            <TableRow>
+                                <TableCell align="center" colSpan={5}>
+                                    <Box my={2} display="flex" justifyContent="center">
+                                        <Pagination
+                                            count={setPageCount()}
+                                            color="primary"
+                                            selected={false}
+                                            defaultPage={page}
+                                            page={page}
+                                            onChange={(e,page)=>{
+                                                history.push(`/contact-us?page=${page}`)
+                                                setPage(page)
+                                            }}
+                                        />
+                                    </Box>
+                                </TableCell>
+                                <TableCell align="center">
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={async ()=>{
+                                            await loginCheck(token)
+                                        }}
+                                    >
+                                    글 작성
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
                         </TableBody>
                     </Table>
                 </TableContainer>
-                <Button
-                    variant="contained"
-                    color="primary"
-                    onClick={async ()=>{
-                        await loginCheck(token)
-                    }}
-                >
-                    글 작성
-                </Button>
             </Grid>
             <Grid item xs={4}>
                 <TableContainer component={Paper}>
@@ -126,6 +172,7 @@ const Contact = () => {
                 </TableContainer>
             </Grid>
         </Grid>
+        </div>
     )
 };
 
