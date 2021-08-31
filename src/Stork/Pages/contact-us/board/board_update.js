bdcd3cde3ad3d904bb122c9bae6a48ef4eea83a1
salvable <salvable@kakao.com>
2021-08-31 20:55:00 +0900

    updateForm 작성 완료

---
+++ src/Stork/Pages/contact-us/board/board_update.js
@@ -0,0 +1,157 @@
+import React, {useEffect, useState} from 'react';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Checkbox from '@material-ui/core/Checkbox';
+import Grid from '@material-ui/core/Grid';
+import {Table, TableBody, TableCell, TableContainer, TableHead, TableRow} from "@material-ui/core";
+import Paper from "@material-ui/core/Paper";
+import TextareaAutosize from '@material-ui/core/TextareaAutosize';
+import Contact from "../contact-us";
+import axios from "axios";
+import {useHistory} from "react-router";
+
+const BoardUpdate = ({match}) => {
+
+    const [name,setName] = useState("")
+    const [content,setContent] = useState("")
+    const [id,setId] = useState("")
+    const [password,setPassword] = useState("")
+    const [file,setFile] = useState(null)
+
+    const history = useHistory()
+    const token = localStorage.getItem('accessToken')
+
+    useEffect(() => {
+        async function getBoard(){
+            const response = await axios.get(`http://localhost:3000/board/${match.params.boardId}`,{
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }})
+            setName(response.data.board.name)
+            setContent(response.data.board.content)
+            setId(response.data.board.writer)
+            setPassword(response.data.board.password)
+        }
+        getBoard()
+    }, []);
+
+    const fileChange = (e) =>{
+        setFile(e.target.files[0])
+    }
+
+    const cancel = () =>{
+        if(window.confirm("입력을 취소하고 이전페이지로 돌아가시겠습니까?")) {
+            history.goBack()
+        }
+    }
+
+    return (
+        <Grid container spacing={6} style={{marginTop: 1, display: 'flex'}}>
+            <Grid item xs={8} >
+                <TableContainer component = {Paper}>
+                    <Table aria-label="simple table" >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell align="left" colSpan={4} style={{justifyContent: 'center'}}><h1>글 수정</h1></TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            <TableRow>
+                                <TableCell style={{width: "50%"}}>
+                                    <TextField
+                                        variant="outlined"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="제목"
+                                        label="제목"
+                                        placeholder="제목"
+                                        id="name"
+                                        value={name}
+                                        autoComplete="current-password"
+                                        onChange={(e)=>{
+                                            setName(e.target.value)
+                                        }}
+                                    />
+                                </TableCell>
+                                <TableCell style={{width: "20%"}}>
+                                    <TextField
+                                        variant="filled"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="ID"
+                                        label="ID"
+                                        placeholder="ID"
+                                        disabled = {true}
+                                        type="ID"
+                                        id="ID"
+                                        color = "primary"
+                                        value={id}
+                                        autoComplete="current-password"
+                                    />
+                                </TableCell>
+                                <TableCell style={{width: "20%"}}>
+                                    <TextField
+                                        variant="filled"
+                                        margin="normal"
+                                        required
+                                        fullWidth
+                                        name="password"
+                                        label="password"
+                                        disabled = {true}
+                                        placeholder="password"
+                                        type="password"
+                                        id="password"
+                                        value={password}
+                                        autoComplete="current-password"
+                                    />
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell colSpan={4}>
+                                    <TextareaAutosize
+                                        minRows = "25"
+                                        style={{width: "100%"}}
+                                        value={content}
+                                        onChange={(e)=> {
+                                            setContent(e.target.value)
+                                        }}>
+                                    </TextareaAutosize>
+                                </TableCell>
+                            </TableRow>
+                            <TableRow>
+                                <TableCell colSpan={2}>
+                                    <input
+                                        type="file"
+                                        multiple onChange={(e)=>{
+                                        fileChange(e)
+                                    }}
+                                    />
+                                </TableCell>
+                                <TableCell colSpan={2} align="right">
+                                    <Button
+                                        variant="contained"
+                                        color="secondary"
+                                        onClick={()=>{
+                                            cancel()
+                                        }}>취소</Button>
+                                    <Button
+                                        variant="contained"
+                                        color="primary"
+                                        onClick={async()=>{
+
+                                        }}>
+                                        글수정
+                                    </Button>
+                                </TableCell>
+                            </TableRow>
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </Grid>
+        </Grid>
+    );
+}
+
+export default BoardUpdate
